809610c9c7395445348f7be54045b16d46614131
Alex Bepple <alex@bepple.de>
2012-10-26 19:59:21 +0200

    ~ write spec more conveniently

--- spec/simple_url_spec.rb
+++ spec/simple_url_spec.rb
@@ -2,21 +2,37 @@
 
 require 'url'
 
+def simplify test_data
+  Url.new(test_data[0]).simplify.should == test_data[1]
+end
+
 describe 'Url Simplifier' do
   describe 'removes everything after a number' do
     it 'from a LH url' do
-      Url.new('http://lifehacker.com/5926574/screenleap-for-gmail-offers-one+click-screen-sharing-from-your-inbox-or-google-contacts').simplify.should == 'http://lifehacker.com/5926574'
+      simplify %w{
+        http://lifehacker.com/5926574/screenleap-for-gmail-offers-one+click-screen-sharing-from-your-inbox-or-google-contacts
+        http://lifehacker.com/5926574
+      }
     end
     it 'from a The Changelog url' do
-      Url.new('http://thechangelog.com/post/26907189672/kineticjs-html5-canvas-drawing-made-easy').simplify.should == 'http://thechangelog.com/post/26907189672'
+      simplify %w{
+        http://thechangelog.com/post/26907189672/kineticjs-html5-canvas-drawing-made-easy
+        http://thechangelog.com/post/26907189672
+      }
     end
   end
   
   it 'removes crap from Amazon url' do
-    Url.new('http://www.amazon.de/gp/product/B0088CG2S6/ref=s9_newr_gw_d80_g340_ir03?pf_rd_m=A3JWKAKR8XB7XF&pf_rd_s=center-2&pf_rd_r=1XKGD5MM7W03Y4WTZPDE&pf_rd_t=101&pf_rd_p=463375173&pf_rd_i=301128').simplify.should == 'http://www.amazon.de/gp/product/B0088CG2S6'
+    simplify %w{
+      http://www.amazon.de/gp/product/B0088CG2S6/ref=s9_newr_gw_d80_g340_ir03?pf_rd_m=A3JWKAKR8XB7XF&pf_rd_s=center-2&pf_rd_r=1XKGD5MM7W03Y4WTZPDE&pf_rd_t=101&pf_rd_p=463375173&pf_rd_i=301128
+      http://www.amazon.de/gp/product/B0088CG2S6
+    }
   end
 
   it 'removes utm_source crap' do
-    Url.new('http://osxdaily.com/2012/07/18/convert-dmg-to-cdr-or-iso-with-disk-utility/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+osxdaily+%28OS+X+Daily%29').simplify.should == 'http://osxdaily.com/2012/07/18/convert-dmg-to-cdr-or-iso-with-disk-utility'
+    simplify %w{
+      http://osxdaily.com/2012/07/18/convert-dmg-to-cdr-or-iso-with-disk-utility/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+osxdaily+%28OS+X+Daily%29
+      http://osxdaily.com/2012/07/18/convert-dmg-to-cdr-or-iso-with-disk-utility
+    }
   end
 end
