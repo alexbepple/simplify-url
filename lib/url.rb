f5bc9667f5324e659da32782d5607f1da627c7f9
Alex Bepple <alex@bepple.de>
2013-01-30 20:42:43 +0100

    + Stack Overflow

--- lib/url.rb
+++ lib/url.rb
@@ -4,7 +4,7 @@ class Url
     '(.+spiegel.+/).+(a-\d+.*)',
     '(.+spiegel.+/[\d,]+.html)',
     '(.+amazon.+/product/[A-Z0-9]+)',
-    '(.*(?:lifehacker|changelog|gizmodo).+/\d+)',
+    '(.*(?:lifehacker|changelog|gizmodo|stackoverflow).+/\d+)',
     '(.+)/\?utm_source',
     '(.+)'
   ]
