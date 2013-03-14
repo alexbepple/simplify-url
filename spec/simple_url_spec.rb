71021166eb642ae4140bc23403567a95045a18c6
Alex Bepple <alex@bepple.de>
2013-03-14 09:32:56 +0100

    ~ simplify tests

--- spec/simple_url_spec.rb
+++ spec/simple_url_spec.rb
@@ -6,64 +6,64 @@ describe 'Url Simplifier' do
 
   it "leaves urls in peace when nuthin' to do" do
     leave_unchanged 'http://a.b.c'
-    leave_unchanged 'http://www.spiegel.de/netzwelt/technologie/0,1518,370532,00.html'
-    leave_unchanged 'https://mail.google.com/mail/u/0/#all/13879cf0284080c0'
+    leave_unchanged 'http://www.spiegel.de/cat/subcat/0,42,42,00.html'
+    leave_unchanged 'https://mail.google.com/mail/u/0/#all/42f'
   end
 
   describe 'removes everything after a number' do
     it 'for Lifehacker' do
       simplify %w{
-        http://lifehacker.com/5926574/screenleap-for-gmail-offers-one+click-screen-sharing-from-your-inbox-or-google-contacts
-        http://lifehacker.com/5926574
+        http://lifehacker.com/42/foo
+        http://lifehacker.com/42
       }
     end
     it 'for Gizmodo' do
       simplify %w{
-        http://gizmodo.com/5341915/android-hacking-for-the-masses
-        http://gizmodo.com/5341915
+        http://gizmodo.com/42/foo
+        http://gizmodo.com/42
       }
     end
     it 'for Apple forums' do
       simplify %w{
-        https://discussions.apple.com/message/16611473#16611473 
-        https://discussions.apple.com/message/16611473
+        https://discussions.apple.com/message/42#42
+        https://discussions.apple.com/message/42
       }
     end
     it 'even if there is no protocol in the url' do
       simplify %w{
-        gizmodo.com/5341915/android-hacking-for-the-masses
-        gizmodo.com/5341915
+        gizmodo.com/42/foo
+        gizmodo.com/42
       }
     end
   end
   
   it 'removes utm_source crap' do
     simplify %w{
-      http://osxdaily.com/2012/07/18/convert-dmg-to-cdr-or-iso-with-disk-utility/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+osxdaily+%28OS+X+Daily%29
-      http://osxdaily.com/2012/07/18/convert-dmg-to-cdr-or-iso-with-disk-utility
+      http://osxdaily.com/2012/07/18/foo/?utm_source=foo
+      http://osxdaily.com/2012/07/18/foo
     }
   end
 
   describe 'removes unnecessary bits' do
     it 'for App Store' do
       simplify %w{
-        http://itunes.apple.com/app/calendars-google-calendar/id371434886
-        http://itunes.apple.com/app/id371434886
+        http://itunes.apple.com/app/foo/id42
+        http://itunes.apple.com/app/id42
       }
     end
     it 'for Spiegel Online' do
       simplify %w{
-        http://www.spiegel.de/kultur/musik/debuetalben-von-teed-lemonade-christian-loeffler-neue-techno-impulse-a-846509.html
-        http://www.spiegel.de/kultur/musik/a-846509.html
+        http://www.spiegel.de/kultur/musik/foo-a-42.html
+        http://www.spiegel.de/kultur/musik/a-42.html
       }
     end
     it 'for Amazon' do
       simplify %w{
-        http://www.amazon.de/gp/product/B0088CG2S6/ref=s9_newr_gw_d80_g340_ir03?pf_rd_m=A3JWKAKR8XB7XF&pf_rd_s=center-2&pf_rd_r=1XKGD5MM7W03Y4WTZPDE&pf_rd_t=101&pf_rd_p=463375173&pf_rd_i=301128
+        http://www.amazon.de/gp/product/B0088CG2S6/foo
         http://www.amazon.de/dp/B0088CG2S6
       }
       simplify %w{
-        http://www.amazon.de/Philips-LED-Birne-Tropfenform-matt-19564100/dp/B0085WGRA2/ref=sr_1_5?s=lighting&ie=UTF8&qid=1354224824&sr=1-5
+        http://www.amazon.de/foo/dp/B0085WGRA2/foo
         http://www.amazon.de/dp/B0085WGRA2
       }
     end
@@ -73,20 +73,20 @@ describe 'Url Simplifier' do
   describe 'references Gmail messages in a stable manner' do
     it 'from inbox' do
       simplify %w{
-        https://mail.google.com/mail/u/0/#inbox/13879cf0284080c0 
-        https://mail.google.com/mail/u/0/#all/13879cf0284080c0
+        https://mail.google.com/mail/u/0/#inbox/42f
+        https://mail.google.com/mail/u/0/#all/42f
       }
     end
     it 'from search' do
       simplify %w{
-        https://mail.google.com/mail/u/1/#search/funnel/138e2def960ca553 
-        https://mail.google.com/mail/u/1/#all/138e2def960ca553
+        https://mail.google.com/mail/u/1/#search/foo/42f
+        https://mail.google.com/mail/u/1/#all/42f
       }
     end
     it 'from label' do
       simplify %w{
-        https://mail.google.com/mail/u/0/#label/act/13d0ffede60d22d1
-        https://mail.google.com/mail/u/0/#all/13d0ffede60d22d1
+        https://mail.google.com/mail/u/0/#label/foo/42f
+        https://mail.google.com/mail/u/0/#all/42f
       }
     end
   end
