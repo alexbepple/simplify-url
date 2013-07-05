851f34bc11ec7993f76062fc131a8e3ee1e90eca
Alex Bepple <alex@bepple.de>
2013-07-05 20:46:24 +0200

    ~ uniformly represent substitution rules

--- lib/url.rb
+++ lib/url.rb
@@ -1,6 +1,11 @@
 class UrlSimplifier
 
   def initialize
+    @substitution_patterns = [
+      [/mail.google/, %r{#(search|label)/[^/]+}, '#all'],
+      [/mail.google/, %r{#[^/]+},                '#all'],
+      [/amazon/, %r{gp/product}, 'dp'],
+    ]
     @simplification_patterns = [
       '(.+apple.+/app/).*(id\d+)',
       '(.+spiegel.+/).+(a-\d+.*)',
@@ -16,23 +21,16 @@ class UrlSimplifier
   end
 
   def simplify url
+    suitable_substitution_patterns = @substitution_patterns.select { |p| url=~Regexp.new(p[0])}
+    suitable_substitution_patterns.each { |p| url = url.sub(p[1], p[2])}
+
     if url.match(/mail.google/) 
-      [
-        %r{#(search|label)/[^/]+}, 
-        %r{#[^/]+}
-      ].each do |unstable_reference|
-        url.sub!(unstable_reference, '#all')
-      end
       return url
     end
 
     suitable_patterns = @simplification_patterns.select { |pattern| url =~ Regexp.new(pattern) }
     regex_for_best_pattern = Regexp.new(suitable_patterns[0])
     url = regex_for_best_pattern.match(url).captures.join()
-
-    if url.match(/amazon/)
-      url.sub!(%r{gp/product}, 'dp')
-    end
 
     url
   end
