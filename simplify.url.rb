7abf03a7c74354a46b15616eba2d8ff65ada8ed1
Alex Bepple <alex@bepple.de>
2014-09-11 19:29:15 +0200

    simplify URLs, not more

--- simplify.url.rb
+++ simplify.url.rb
@@ -3,15 +3,6 @@
 project_dir = File.dirname(File.realpath(__FILE__))
 $LOAD_PATH.unshift File.join(project_dir, 'lib')
 
-
-def get_clipboard
-  IO.popen('pbpaste', 'r+').read
-end
-def write_to_clipboard text
-  IO.popen('pbcopy', 'w').print text
-end
-
 require 'url'
-write_to_clipboard UrlSimplifier.new().simplify(get_clipboard)
+puts UrlSimplifier.new().simplify(ARGV[0])
 
-puts get_clipboard
