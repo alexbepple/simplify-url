16c87519459cc32698485d1ffeceb368f53dc431
Alex Bepple <alex@bepple.de>
2018-06-10 01:13:16 +0200

    . simplify

--- src/clean-url.js
+++ src/clean-url.js
@@ -1,18 +1,25 @@
 import * as r from "ramda";
 
-export default x => {
-  const url = new URL(r.identity(x));
+const toPathComponents = r.split("/");
+const toPathname = r.pipe(
+  r.join("/"),
+  r.concat("/")
+);
+
+const dropEverythingBeforeDp = r.dropWhile(r.complement(r.equals("dp")));
+const cleanAmazonPathname = r.pipe(
+  toPathComponents,
+  dropEverythingBeforeDp,
+  r.take(2),
+  toPathname
+);
+
+export default dirtyUrlString => {
+  const url = new URL(dirtyUrlString);
   if (url.hostname.indexOf("amazon") > -1) {
-    const pathSegments = url.pathname.split("/");
-
-    const indexOfDp = pathSegments.indexOf("dp");
-    pathSegments.splice(0, indexOfDp);
-
-    pathSegments.splice(2);
-
-    url.pathname = "/" + pathSegments.join("/");
+    url.pathname = cleanAmazonPathname(url.pathname);
     url.search = "";
     return url.toString();
   }
-  return x;
+  return dirtyUrlString;
 };
