0490a5ae4f795fc9eea800e942c328d50d7978f1
Alex Bepple <alex@bepple.de>
2018-06-09 19:56:42 +0200

    $ copy current url to clipboard

--- src/background.js
+++ src/background.js
@@ -1,9 +1,13 @@
+var textToPutOnClipboard;
+
 document.addEventListener("copy", function(e) {
-  var textToPutOnClipboard = "some text which should appear in clipboard";
   e.clipboardData.setData("text/plain", textToPutOnClipboard);
   e.preventDefault();
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  document.execCommand("copy");
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    textToPutOnClipboard = tabs[0].url;
+    document.execCommand("copy");
+  });
 });
